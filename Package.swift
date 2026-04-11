db69b3408146a12adbf3546ac476e6061def0950
zhanggaoqi <zhanggaoqi@aobi.com>
2026-04-11 18:29:14 +0800

    [Branch Release] v2.0.1-dev-1420704 - develop
    - 更新所有 XCFrameworks 到 2.0.1-dev-1420704
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "BTLoganManager",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTLoganManager/2.0.1-dev-1407380/BTLoganManager.xcframework.zip",
-            checksum: "2144df70a9e9f8bbb14060f1ce70b608538ba5aa416d0bc8d8d68c9a2efe1fe8"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/BTLoganManager/2.0.1-dev-1420704/BTLoganManager.xcframework.zip",
+            checksum: "c34acddde4128d63800a257386eda96c1ea788efc12020693e95799bab4a221b"
         ),
         
         // ========== Bundle Resources ==========
